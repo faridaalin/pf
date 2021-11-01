b6f35b302174e77dd015114abed7e8f1aed2bc86
Farida <farida.alin@yahoo.com>
2021-11-01 22:49:35 +0100

    wip

--- helpers/validateInputFields.js
+++ helpers/validateInputFields.js
@@ -1,9 +1,3 @@
-function validateEmail(val) {
-  const regEx =
-    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return regEx.test(val);
-}
-
 export const validateInputFields = (tag) => {
   const inputs = document.querySelectorAll(tag);
   const inputsArr = [...inputs];
@@ -17,19 +11,22 @@ export const validateInputFields = (tag) => {
       inputsArr[i].classList.add('is-invalid');
       inputsArr[i].nextSibling.nextElementSibling.innerHTML = 'Field is empty!';
       isValid = false;
-    } else if (
-      inputsArr[i].id === 'email' &&
-      inputsArr[i].value.length > 0 &&
-      !regExEmail.test(inputsArr[i].value)
-    ) {
-      document.querySelector('.email-error').innerHTML = 'Invalid email';
-      inputsArr[i].classList.add('is-invalid');
-      isValid = false;
+    } else if (inputsArr[i].id === 'email' && inputsArr[i].value.length > 0) {
+      if (!regExEmail.test(inputsArr[i].value)) {
+        console.log('Here');
+        inputsArr[i].nextSibling.nextElementSibling.innerHTML = 'Invalid email';
+        inputsArr[i].classList.add('is-invalid');
+        isValid = false;
+      } else {
+        console.log('here');
+        console.dir(inputsArr[i]);
+        inputsArr[i].nextSibling.nextElementSibling.innerHTML = '';
+        inputsArr[i].className = 'input email is-valid';
+      }
     } else {
       inputsArr[i].classList.remove('is-invalid');
       inputsArr[i].classList.add('is-valid');
       inputsArr[i].nextSibling.nextElementSibling.innerHTML = '';
-      document.querySelector('.email-error').innerHTML = '';
     }
   }
 
